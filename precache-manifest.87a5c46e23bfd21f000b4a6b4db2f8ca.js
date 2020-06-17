3cbefb00a95ad1ea3c116bbd5270a246fc99fd26
Peter Matik <matik.peter@gmail.com>
2020-06-17 17:33:38 +0200

    Updates

---
+++ precache-manifest.87a5c46e23bfd21f000b4a6b4db2f8ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fde5717637790b00bd5e56d5637837",
+    "url": "/expenses-planner/index.html"
+  },
+  {
+    "revision": "e161a1796212907b54c1",
+    "url": "/expenses-planner/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ec29c0cdd6190efef0ce",
+    "url": "/expenses-planner/static/css/main.7e6f8cb3.chunk.css"
+  },
+  {
+    "revision": "e161a1796212907b54c1",
+    "url": "/expenses-planner/static/js/2.ca843bd8.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/expenses-planner/static/js/2.ca843bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec29c0cdd6190efef0ce",
+    "url": "/expenses-planner/static/js/main.a66babec.chunk.js"
+  },
+  {
+    "revision": "3c36654eb3c925b33fd3",
+    "url": "/expenses-planner/static/js/runtime-main.bce659cd.js"
+  }
+]);
