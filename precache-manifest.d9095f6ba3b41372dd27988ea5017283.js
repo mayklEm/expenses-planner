da1cdf3ec4655a492f5a97c58928cd4936122b14
Peter Matik <matik.peter@gmail.com>
2020-06-17 18:33:05 +0200

    Updates

--- precache-manifest.d9095f6ba3b41372dd27988ea5017283.js
+++ precache-manifest.d9095f6ba3b41372dd27988ea5017283.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8359540ddc16b1e1003978468bc6fbbe",
+    "revision": "cc56de155d0571678d944004bbcbc985",
     "url": "/expenses-planner/index.html"
   },
   {
-    "revision": "43b0ab7d76b52cc22c96",
+    "revision": "fa7cf056797038be32dc",
     "url": "/expenses-planner/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "46525518d2fde3d6d9af",
+    "revision": "36d23b3c1b6e0f3085c1",
     "url": "/expenses-planner/static/css/main.7e6f8cb3.chunk.css"
   },
   {
-    "revision": "43b0ab7d76b52cc22c96",
-    "url": "/expenses-planner/static/js/2.71f57285.chunk.js"
+    "revision": "fa7cf056797038be32dc",
+    "url": "/expenses-planner/static/js/2.68c875bf.chunk.js"
   },
   {
     "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
-    "url": "/expenses-planner/static/js/2.71f57285.chunk.js.LICENSE.txt"
+    "url": "/expenses-planner/static/js/2.68c875bf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "46525518d2fde3d6d9af",
-    "url": "/expenses-planner/static/js/main.74f33ee5.chunk.js"
+    "revision": "36d23b3c1b6e0f3085c1",
+    "url": "/expenses-planner/static/js/main.dba185db.chunk.js"
   },
   {
     "revision": "3c36654eb3c925b33fd3",
